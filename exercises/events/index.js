229d63185156b1e324e59f72b217f2ac99bb8aa3
dreak603 <dreak603@gmail.com>
2019-11-14 00:15:25 +0800

    13 commit

---
+++ exercises/events/index.js
@@ -0,0 +1,29 @@
+// --- Directions
+// Create an 'eventing' library out of the
+// Events class.  The Events class should
+// have methods 'on', 'trigger', and 'off'.
+
+class Events {
+    constructor() {
+        this.events = {};
+    }
+
+    // Register an event handler
+    on(eventName, callback) {
+        this.events[eventName] ? this.events[eventName].push(callback) : this.events[eventName] = [callback];
+    }
+
+    // Trigger all callbacks associated
+    // with a given eventName
+    trigger(eventName) {
+        this.events[eventName] ? this.events[eventName].map(cb => cb()) : null;
+    }
+
+    // Remove all event handlers associated
+    // with the given eventName
+    off(eventName) {
+        delete this.events[eventName];
+    }
+}
+
+module.exports = Events;
--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,18 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    // implement bubble sort
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    // return 
+    return arr;
 }
 
 function selectionSort(arr) {
