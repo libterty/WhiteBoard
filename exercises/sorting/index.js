1056a4b190f7386fb80411349839463d2d30fcd4
dreak603 <dreak603@gmail.com>
2019-11-18 20:02:06 +0800

    14 commit

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,8 +16,31 @@ function bubbleSort(arr) {
     return arr;
 }
 
-function selectionSort(arr) {
+// From i = 0 to < arr.length
+// Assume the element at i is the least in the array, assign i to indexOfMin
+// For j from i + 1 to end to array
+// See if there is an element with lower value
+// If there is record its index
+// If the index of the current element and the index of the lowest element is not the same swap them
 
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let indexOfMin = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+        // swap logic
+        if (indexOfMin !== i) {
+            let lesser = arr[indexOfMin];
+            arr[indexOfMin] = arr[i];
+            arr[i] = lesser;
+        }
+    }
+
+    return arr;
 }
 
 function mergeSort(arr) {
