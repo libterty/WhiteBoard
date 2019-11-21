ef0ecf007c04890e25ce4ecf56a72a9e1a85a241
dreak603 <dreak603@gmail.com>
2019-11-21 14:41:20 +0800

    15 commit

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -43,12 +43,39 @@ function selectionSort(arr) {
     return arr;
 }
 
+// example [9, 5, 3, 10]
 function mergeSort(arr) {
-
+    if (arr.length === 1) {
+        return arr;
+    }
+    // divided arr into two
+    const center = Math.floor(arr.length / 2); // mergeSort1 [9, 5, 3, 10] // mergeSort1 [9, 5] [3, 10]
+    const left = arr.slice(0, center); // Left1 [9, 5] // left2 [9] [3]
+    const right = arr.slice(center); // right1 [3, 10] // right2 [5] [10]
+    // recursion 
+    // passing Left1 && Right1 then recurse
+    // pasing Left2 && Right2 then recurse then length === 1
+    // call merge function
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+    // create a results array
+    const result = [];
 
+    // while there are still in both arrarys
+    while (left.length && right.length) {
+        // if the first element at the left half is less then first at the right
+        if (left[0] < right[0]) {
+            // shift the element from left into result arr
+            result.push(left.shift());
+        } else {
+            // else shift the right into result arr
+            result.push(right.shift());
+        }
+    }
+    // take everything from the array the still has stuff in it and put it in results
+    return [...result, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
